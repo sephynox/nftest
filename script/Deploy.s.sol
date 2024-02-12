16dfe2d51060e8599f0459a6c9835a46ec248685
sephynox <tan@wahid.email>
2024-02-11 16:12:21 -0800

    Test the deploy script.

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -6,25 +6,54 @@ import {Reward} from "../src/Reward.sol";
 import {RewardNFT} from "../src/RewardNFT.sol";
 
 contract DeployScript is Script {
+    /**
+     * The deploy script is temporary owner
+     */
+    address tempOwner = address(this);
+    /**
+     * The intended owner of the deployed contracts
+     */
+    address public intendedOwner;
+    /**
+     * The Reward contract
+     */
     Reward public reward;
+    /**
+     * The RewardNFT contract
+     */
     RewardNFT public rewardNFT;
 
-    function setUp() public {}
+    function setUp() public {
+        /**
+         * Set intended owner as the sender
+         */
+        intendedOwner = msg.sender;
+    }
 
     function run() public {
         /**
-         * Deploy Reward
+         * Deploy Reward with DeployScript as owner
          */
-        reward = new Reward(msg.sender);
+        reward = new Reward(tempOwner);
         /**
-         * Deploy RewardNFT
+         * Deploy RewardNFT with DeployScript as owner
          */
-        rewardNFT = new RewardNFT(msg.sender, address(reward));
+        rewardNFT = new RewardNFT(tempOwner, address(reward));
 
         /**
          * Set RewardNFT to Reward
          */
         reward.setRewardNFT(address(rewardNFT));
+
+        /**
+         * Transfer ownership of Reward to intendedOwner
+         */
+        reward.transferOwnership(intendedOwner);
+        /**
+         * Transfer ownership of RewardNFT to intendedOwner
+         */
+        rewardNFT.transferOwnership(intendedOwner);
+
         /**
          * Broadcast
          */
---
+++ test/Deploy.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DeployScript} from "../script/Deploy.s.sol";
+
+contract DeployScriptTest is Test {
+    DeployScript public script;
+
+    function setUp() public {
+        script = new DeployScript();
+    }
+
+    function testRun() public {
+        script.setUp();
+        script.run();
+
+        assertEq(script.reward().owner(), address(this), "Reward contract owner is not correct");
+        assertEq(script.rewardNFT().owner(), address(this), "RewardNFT contract owner is not correct");
+        assertEq(script.reward().rewardNFT(), address(script.rewardNFT()), "Reward contract's RewardNFT is not correct");
+    }
+}
